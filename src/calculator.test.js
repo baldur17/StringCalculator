5c89ed668e7d580a0a5c698a01ae7b42e4ba31ca
baldur17 <baldur17@ru.is>
2018-10-10 01:52:45 +0000

    tests refactored

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,18 +4,18 @@ it("should return zero on an empty string", () => {
   expect(add("")).toBe(0);
 });
 
-it("should return zero on an empty string", () => {
+it("should return 4", () => {
   expect(add("4")).toBe(4);
 });
 
-it("should return zero on an empty string", () => {
+it("should return 9", () => {
   expect(add("4,5")).toBe(9);
 });
 
-it("should return zero on an empty string", () => {
+it("should return 6", () => {
   expect(add("1,2,3")).toBe(6);
 });
 
-it("should return zero on an empty string", () => {
+it("should return 6", () => {
   expect(add("1\n2,3")).toBe(6);
 });
