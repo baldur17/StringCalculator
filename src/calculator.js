fdc98b0aeb5a176316b15045750728ac1dbefad2
baldur17 <baldur17@ru.is>
2018-10-10 01:34:49 +0000

    refactoring

--- src/calculator.js
+++ src/calculator.js
@@ -4,13 +4,17 @@ function add (number){
   }
   if(number.includes(",")){
     var numberArray = number.split(",");
-    var retValue = 0;
-    for(var i = 0; i < numberArray.length; i++){
-      retValue = retValue + parseInt(numberArray[i]);
-    }
-    return retValue;
+    return sum(numberArray);
   }
   return parseInt(number);
+}
+
+function sum(numberArray){
+  var retValue = 0;
+  for(var i = 0; i < numberArray.length; i++){
+    retValue = retValue + parseInt(numberArray[i]);
+  }
+  return retValue;
 }
 
 module.exports = add;
