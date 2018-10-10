feced87e7adf8c65a75dad347a43fe3b267c3074
baldur17 <baldur17@ru.is>
2018-10-10 01:24:59 +0000

    implemented for two numbers

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -7,3 +7,7 @@ it("should return zero on an empty string", () => {
 it("should return zero on an empty string", () => {
   expect(add("4")).toBe(4);
 });
+
+it("should return zero on an empty string", () => {
+  expect(add("4,5")).toBe(9);
+});
