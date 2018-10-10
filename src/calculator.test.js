dd020525cc68057ea8a0d535f7964ed92a1f579a
baldur17 <baldur17@ru.is>
2018-10-10 01:20:09 +0000

    implemented for one number

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("./calculator");
 it("should return zero on an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+it("should return zero on an empty string", () => {
+  expect(add("4")).toBe(4);
+});
