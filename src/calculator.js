d9b6afb77b0defd598d3b946f30b117ba26f697c
baldur17 <baldur17@ru.is>
2018-10-11 14:29:03 +0000

    impemented not allowing negative numbers

--- src/calculator.js
+++ src/calculator.js
@@ -2,8 +2,14 @@ function add (number){
   if(number == ""){
     return 0;
   }
+  if(parseInt(number) < 0)
+  {
+    throw "Negatives not allowed: " + number;
+  }
+
   if(number.includes(",") || number.includes("\n")){
     var numberArray = number.replace(/\n/g, ",").split(",");
+    negativenumbers(numberArray);
     return sum(numberArray);
   }
   return parseInt(number);
@@ -11,10 +17,23 @@ function add (number){
 
 function sum(numberArray){
   var retValue = 0;
+
   for(var i = 0; i < numberArray.length; i++){
-    retValue = retValue + parseInt(numberArray[i]);
+      retValue = retValue + parseInt(numberArray[i]);
   }
   return retValue;
+}
+
+function negativenumbers(numbers){
+  var negativeValues = numbers.filter(negativecheck);
+
+  if(negativeValues.length != 0){
+    throw "Negatives not allowed: " + negativeValues;
+  }
+}
+
+function negativecheck(number){
+  return number < 0;
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -19,3 +19,9 @@ it("should return 6", () => {
 it("should return 6", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('should throw negative error: -2', () => {
+  expect(() => {
+    add('1,-2,3');
+  }).toThrow();
+});
